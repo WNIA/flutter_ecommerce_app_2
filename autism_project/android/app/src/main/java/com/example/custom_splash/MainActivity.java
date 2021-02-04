4f5d7e83f8d330e702bb0ef22957d3385bac66be
WNIA <wnia22@gmail.com>
2021-02-04 13:12:53 +0600

    splash screen problem 1

---
+++ autism_project/android/app/src/main/java/com/example/custom_splash/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.custom_splash;
+
+import android.content.Context;
+import android.util.Log;
+import androidx.annotation.Nullable;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.SplashScreen;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.view.FlutterMain;
+
+
+public class MainActivity extends FlutterActivity {
+  private static FlutterEngine flutterEngine;
+  @Override
+  public FlutterEngine provideFlutterEngine(Context context) {
+    if (flutterEngine == null) {
+      flutterEngine = new FlutterEngine(context);
+      flutterEngine.getDartExecutor().executeDartEntrypoint(new DartExecutor.DartEntrypoint(
+          FlutterMain.findAppBundlePath(), "main"));
+    }
+    return flutterEngine;
+  }
+
+  @Override
+  public SplashScreen provideSplashScreen() {
+    return new CustomSplashScreen();
+  }
+}
